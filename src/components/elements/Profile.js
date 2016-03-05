93bfd4f66af72998981370ba0dcc5e02ec9ba4ab
Mike Lohmann <mike.lohmann@deck36.de>
2016-03-05 12:04:18 +0100

    Excercise2 - Adding username

--- src/components/elements/Profile.js
+++ src/components/elements/Profile.js
@@ -1,7 +1,13 @@
 import React from 'react'
 import styles from './Profile.scss'
 import { IndexLink } from 'react-router'
+import { connect } from 'react-redux'
 
+@connect(
+  state => ({
+    user_name: state.sendbird.user_name
+  })
+)
 export default class Profile extends React.Component {
 
   render() {
@@ -9,6 +15,7 @@ export default class Profile extends React.Component {
       <nav className="text-center">
         <div className={styles.userContainer}>
           <span className={styles.user}>
+            <p>{this.props.user_name}</p>
             <IndexLink to="/" className={styles.signout} />
           </span>
         </div>
